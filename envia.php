79ca978c6e6cfdca1ea6bd8b3985922d0e15e13d
brunotguima <bruno.guimaraes13@etec.sp.gov.br>
2018-06-08 15:31:47 -0300

    Suporte OK

--- envia.php
+++ envia.php
@@ -1,38 +1,46 @@
 <?php
+require_once("class.phpmailer.php"); 
 $nome = $_POST['name'];
 $email = $_POST['email'];
+$subject = $_POST['subject'];
 $mensagem = $_POST['message'];
-?>
-<?php
-$conteudo .='Nome: ' .$nome;
+if (array_key_exists('arquivo', $_FILES)) {
+$uploadNome = $_FILES['arquivo']['name'];
+$upload = tempnam(sys_get_temp_dir(), hash('sha256', $uploadNome));
+move_uploaded_file($_FILES['arquivo']['tmp_name'], $upload);
+}
+$conteudo ='Nome: ' .$nome;
 $conteudo .='<br/>'.'Email: ' .$email;
+$conteudo .='<br/>'.'Assunto: ' .$subject;
 $conteudo .='<br/>'.'Mensagem: ' .$mensagem;
-require_once("class.phpmailer.php"); //caminho do arquivo da classe do phpmailer
-$mail = new PHPMailer();
+try {
+$mail = new PHPMailer(true);
 $mail->CharSet = 'UTF-8';
-$mail->IsSMTP();		// Ativar SMTP
-$mail->SMTPDebug = 1;		// Debugar: 1 = erros e mensagens, 2 = mensagens apenas
-$mail->SMTPAuth = true;		// Autenticação ativada
-$mail->SMTPSecure = 'ssl';	// SSL REQUERIDO pelo GMail
-$mail->Host = 'smtp-mail.outlook.com';	// SMTP utilizado
-$mail->Port = 587;  		// A porta 587 deverá estar aberta em seu servidor
-$mail->Username = "ariel.consultoria@outlook.com"; // usuário de SMTP
-$mail->Password = "abner2005"; // senha de SMTP
-$mail->From = "ariel.consultoria@outlook.com";
-//coloque aqui o seu correio, para que a autenticação não barre a mensagem
-$mail->FromName = "Contato do site Ariel Consultoria";
-$mail->AddAddress("ariel.consultoria@outlook.com");
-$mail->IsHTML(true); // envio como HTML se 'true'
+$mail->IsSMTP();		
+$mail->SMTPDebug = 0;		
+$mail->SMTPAuth = true;		
+$mail->SMTPSecure = 'ssl';	
+$mail->Host = 'email-ssl.com.br';	
+$mail->Port = '465';  		
+$mail->Username = "contato@pittari.com.br"; 
+$mail->Password = "Contatopittari"; 
+$mail->From = "contato@pittari.com.br";
+$mail->FromName = "Contato do site Pittari";
+$mail->AddAddress("contato@pittari.com.br");
+$mail->IsHTML(true); 
 $mail->Subject = "Mensagem do formulário de Contato";
-$mail->Body = $conteudo; 
-//Verifica se o e-mail foi enviado
-if(!$mail->Send()) {
+$mail->Body = $conteudo;
+if(isset($upload)){
+$mail->AddAttachment($upload, $uploadNome);	
+} 
+$mail->Send(); 
     echo "<script>
-		alert('Mensagem n\u00e3o enviada! Tente novamente ou mais tarde.')
+	alert('Mensagem enviada com sucesso! Em breve responderemos.');
+	window.location.href = 'index.html#three';
 	</script>";
-} else {
+} catch (Exception $e) {
   	echo "<script>
-		alert('Mensagem enviada com sucesso! Em breve estaremos respondendo.');
+		alert('Mensagem n\u00e3o enviada! Tente novamente mais tarde.');
 		window.location.href = 'index.html#three';
 	</script>";
 }
